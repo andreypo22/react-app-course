b5ab1d56a84aeef440febee0b089af9c0f5e85c9
Andrey Potapov <potapov_andrey22@mail.ru>
2025-08-19 13:30:57 +0700

    starting app

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,8 @@
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import MainLayout from "./components/MainLayout/MainLayout";
 import Button from "./components/Button/Button";
-import Counter from "./Counter";
-import List from "./List";
 
 function App() {
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <hr />
-      <Counter />
-      <hr />
-      <List />
-    </>
-  );
+  return;
 }
 
 export default App;
